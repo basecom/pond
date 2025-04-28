d36053b1cb75a54674e0bbe2801b39352f6feea0
Cao Hoang Long Nguyen <c.nguyen@basecom.de>
2025-04-28 22:32:03 +0200

    POND-8: add slots

--- components/lineItem/element/type/CartItemProductInner.vue
+++ components/lineItem/element/type/CartItemProductInner.vue
@@ -15,51 +15,80 @@ const props = withDefaults(
       isLoading?: boolean
     }>(),
     {
-        cartItem: undefined,
-        cartDeliveryPosition: undefined,
-        isLoading: false,
+      cartItem: undefined,
+      cartDeliveryPosition: undefined,
+      isLoading: false,
     },
 );
-const { cartItem, isLoading } = toRefs(props);
+const {cartItem, isLoading} = toRefs(props);
 const {
-    itemOptions,
+  itemOptions,
 } = useCartItem(cartItem);
-const { getFormattedPrice } = usePrice();
+const {getFormattedPrice} = usePrice();
 const {
-    itemTotalPrice,
-    itemRegularPrice,
+  itemTotalPrice,
+  itemRegularPrice,
 } = useCartItem(cartItem);
 const configStore = useConfigStore();
 const showDeliveryTime = configStore.get('core.cart.showDeliveryTime') as boolean;
-console.log(showDeliveryTime);
+
 const emits = defineEmits<{
   isLoading: [boolean]
 }>();
 </script>
 <template>
+  <slot name="wrapper">
     <div class="order-1 mb-4 flex w-5/6 flex-col">
-        <div class="mb-2 w-auto">
-            <CartItemImage :cart-item-image="cartItem.cover?.url" fallback="mdi:image" />
-        </div>
-        <div class="font-bold">
-            <NuxtLinkLocale to="/">
-                {{ cartItem.label }}
-            </NuxtLinkLocale>
-        </div>
-        <div class="my-2 text-xs">
-            <CartItemOptions :cart-item-options="itemOptions" />
-        </div>
-        <div class="mb-2 text-xs">
-            {{ $t('checkout.cartItemInfoId') }}: {{ cartItem.payload.productNumber }}
-        </div>
-        <div class="text-xs" v-if="showDeliveryTime">
-            <CartItemDeliveryPosition :cart-item-delivery-position="cartDeliveryPosition" />
-        </div>
-        <div class="mt-2 text-xs">
-            <CartItemAddToWishlist :referenced-id="cartItem.referencedId" />
-        </div>
+      <div class="mb-2 w-auto">
+        <slot name="cart-image">
+          <CartItemImage :cart-item-image="cartItem.cover?.url" fallback="mdi:image"/>
+        </slot>
+      </div>
+      <div class="font-bold">
+        <slot name="labelWrapper">
+          <NuxtLinkLocale to="/">
+            <slot name="label">
+              {{ cartItem.label }}
+            </slot>
+          </NuxtLinkLocale>
+        </slot>
+      </div>
+      <div class="my-2 text-xs">
+        <slot name="options">
+          <CartItemOptions :cart-item-options="itemOptions"/>
+        </slot>
+      </div>
+      <div class="mb-2 text-xs">
+        <slot name="productNumber">
+          {{ $t('checkout.cartItemInfoId') }}: {{ cartItem.payload.productNumber }}
+        </slot>
+      </div>
+      <div class="text-xs" v-if="showDeliveryTime">
+        <slot name="deliveryPosition">
+          <CartItemDeliveryPosition :cart-item-delivery-position="cartDeliveryPosition"/>
+        </slot>
+      </div>
+      <div class="mt-2 text-xs">
+        <slot name="wishlist">
+          <CartItemAddToWishlist :referenced-id="cartItem.referencedId"/>
+        </slot>
+      </div>
     </div>
-    <div class="order-3 mb-4 flex w-full items-center justify-between"><CartItemQuantity :cart-item="cartItem" @is-loading="(isLoadingEmit: boolean) => {emits('isLoading', isLoadingEmit)}" /></div>
-    <div class="order-5 flex w-full justify-end text-xs"><CartItemUnitPrice :cart-item-unit-price="getFormattedPrice(itemRegularPrice)" /></div>
-    <div class="order-4 flex w-full justify-end"><CartItemTotalPrice :cart-item-total-price="getFormattedPrice(itemTotalPrice)" /></div>
+  </slot>
+  <slot name="quantity">
+    <div class="order-3 mb-4 flex w-full items-center justify-between">
+      <CartItemQuantity :cart-item="cartItem"
+                        @is-loading="(isLoadingEmit: boolean) => {emits('isLoading', isLoadingEmit)}"/>
+    </div>
+  </slot>
+  <slot name="unitPrice">
+    <div class="order-5 flex w-full justify-end text-xs">
+      <CartItemUnitPrice :cart-item-unit-price="getFormattedPrice(itemRegularPrice)"/>
+    </div>
+  </slot>
+  <slot name="totalPrice">
+    <div class="order-4 flex w-full justify-end">
+      <CartItemTotalPrice :cart-item-total-price="getFormattedPrice(itemTotalPrice)"/>
+    </div>
+  </slot>
 </template>
