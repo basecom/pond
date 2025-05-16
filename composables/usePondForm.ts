1f10d0951b0eebb1f2644ee194494417240c73c6
nele sophie <53444487+aheartforspinach@users.noreply.github.com>
2025-05-16 08:25:56 +0200

    feat(account): add account profile (#151)
    * [POND-1] add first components
    
    * [POND-1] add wishlist icon in header
    
    * [POND-1] add shadcn
    
    * [POND-1] push broken stuff
    
    * [POND-1] fix import topic
    
    * [POND-1] add inner and outer
    
    * [POND-5] add button componentm extract login
    
    * [POND-5] add login form
    
    * [POND-5] remove dark variables, replace zinc with gray
    
    * [POND-5] hunt error
    
    * [POND-5] fix issue
    
    * [POND-5] add auto animate
    
    * [POND-5] fix issues
    
    * [POND-5] add password forgotten link
    
    * fix typo
    
    * install ducktory
    
    * add ducktory
    
    * add more stories
    
    * fix accordion
    
    * adjust loogin
    
    * remove return
    
    * revert change
    
    * delete unneeded stuff
    
    * use formatLink
    
    * fix localepath
    
    * start with account
    
    * add account overview
    
    * add breadcrumbs
    
    * add story
    
    * fix indent
    
    * add department
    
    * add forms
    
    * add toast
    
    * fix linter
    
    * add password and email change
    
    * fix snippet
    
    * add comments
    
    * test
    
    * ssr false for account
    
    * remove tests
    
    * fix lang file
    
    * test
    
    * fix more
    
    * rework
    
    * fix last bugs (hopefully)
    
    * code style
    
    * add slot
    
    * add code suggestions
    
    * solve merge conflicts
    
    * code review
    
    * add loading spinner, add new error message, add padidng
    
    * fix name
    
    ---------
    
    Co-authored-by: Jordan Kniest <j.kniest@basecom.de>

---
+++ composables/usePondForm.ts
@@ -0,0 +1,95 @@
+import * as z from 'zod';
+import type { Schemas } from '@shopware/api-client/api-types';
+import { DependencyType, type Dependency } from '../components/ui/auto-form/interface';
+import type { ZodObjectOrWrapped } from '~/components/ui/auto-form/utils';
+
+export const usePondForm = () => {
+    const configStore = useConfigStore();
+
+    const getPersonalDataForm = (customer: Schemas['Customer']): ZodObjectOrWrapped => {
+        let personalDataForm = z.object({});
+
+        // add selection of account type
+        const showAccountType = configStore.get('core.loginRegistration.showAccountTypeSelection') as boolean;
+        if (showAccountType) {
+            personalDataForm = personalDataForm.extend({
+                accountType: z.enum(['business', 'private']).optional().default(customer.accountType),
+            });
+        }
+
+        personalDataForm = personalDataForm.extend({
+            salutationId: z.string().optional(),
+        });
+
+        // add title (if set in the admin)
+        const showTitle = configStore.get('core.loginRegistration.showTitleField') as boolean;
+        if (showTitle) {
+            personalDataForm = personalDataForm.extend({
+                title: z.string().optional().default(customer.title ?? ''),
+            });
+        }
+
+        // add fields we always have (first name, last name)
+        personalDataForm = personalDataForm.extend({
+            firstName: z.string().default(customer.firstName),
+            lastName: z.string().default(customer.lastName),
+        });
+
+        // add company and vat number (if we can select the account type)
+        if (showAccountType) {
+            const companyName = customer.accountType === 'business' ? customer.company : '';
+            const vatIds = customer.accountType === 'business' ? customer.vatIds?.join(', ') : '';
+            personalDataForm = personalDataForm.extend({
+                company: z.string().default(companyName),
+                'vatIds[]': z.string().optional().default(vatIds),
+            });
+        }
+
+        // add birthday (if set in the admin)
+        const showBirthday = configStore.get('core.loginRegistration.showBirthdayField') as boolean;
+        if (showBirthday) {
+            if (customer.birthday) {
+                const birthday = new Date(customer.birthday);
+                const day = birthday.getDate();
+                const month = birthday.getMonth() + 1;
+                const year = birthday.getFullYear();
+                personalDataForm = personalDataForm.extend({
+                    birthdayDay: z.number().optional().default(day),
+                    birthdayMonth: z.number().optional().default(month),
+                    birthdayYear: z.number().optional().default(year),
+                });
+            } else {
+                personalDataForm = personalDataForm.extend({
+                    birthdayDay: z.number().optional(),
+                    birthdayMonth: z.number().optional(),
+                    birthdayYear: z.number().optional(),
+                });
+            }
+        }
+
+        return personalDataForm;
+    };
+
+    // we exclude this one because the return type is defined by shadcn
+    // @ts-nocheck
+    /* eslint-disable */
+    const getPersonalDataDependencies = (): Dependency<{ [x: string]: any; }>[] => [
+        {
+            sourceField: 'accountType',
+            type: DependencyType.HIDES,
+            targetField: 'vatIds[]',
+            when: (accountType: string) => accountType !== 'business',
+        },
+        {
+            sourceField: 'accountType',
+            type: DependencyType.HIDES,
+            targetField: 'company',
+            when: (accountType: string) => accountType !== 'business',
+        },
+    ];
+
+    return {
+        getPersonalDataForm,
+        getPersonalDataDependencies,
+    };
+};
