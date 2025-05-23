b49d7c3b7ff5eadac7041db6259f6e6c855654b4
mloeffler <m.loeffler@basecom.de>
2025-05-23 07:55:40 +0200

    remove comments

--- server/routes/account/login/imitate-customer.ts
+++ server/routes/account/login/imitate-customer.ts
@@ -1,76 +1,3 @@
-/*
-import { createAPIClient } from "@shopware/api-client";
-import type { operations } from "#shopware";
-
-export default defineEventHandler(async (event) => {
-    // setTimeout(() => {
-    //
-    // },3000);
-    const body = await readBody(event);
-    if (!body.customerId || !body.token || !body.userId) {
-        console.log(!body.customerId, !body.token, !body.userId);
-        await sendRedirect(event, "/", 400);
-        return;
-    }
-
-    const runtimeConfig = useRuntimeConfig();
-
-    const apiClient = createAPIClient<operations>({
-        baseURL: `${runtimeConfig.public.pond.shopwareEndpoint}/store-api`,
-        accessToken: runtimeConfig.public.pond.accessToken,
-    });
-    console.log('test 1');
-
-    const response = await apiClient.invoke(
-        "imitateCustomerLogin post /account/login/imitate-customer",
-        {
-            body: {
-                customerId: body.customerId,
-                token: body.token,
-                userId: body.userId,
-            },
-        },
-    );
-    console.log('test 2', response);
-
-    // if (!apiClient.defaultHeaders["contextToken"]) {
-    //     await sendRedirect(event, "/", 400);
-    //     return;
-    // }
-
-    console.log('test 3');
-
-    setCookie(
-        event,
-        "contextToken",
-        apiClient.defaultHeaders["contextToken"],
-    );
-    console.log('test 4', apiClient.defaultHeaders["contextToken"]);
-
-    await sendRedirect(event, "/account", 200);
-});
-
-*/
-//
-// import { readBody, sendRedirect } from 'h3';
-//
-// export default defineEventHandler(async event => {
-//     // This server route ist called from SW admin
-//     if (event.method === 'POST') {
-//         const body = await readBody(event);
-//         const token = body.token;
-//         const userId = body.userId;
-//         const customerId = body.customerId;
-//
-//         // Redirect to dedicated page, to call login store api
-//         await sendRedirect(
-//             event,
-//             `/imitate-customer/?token=${token}&userId=${userId}&customerId=${customerId}`,
-//             200,
-//         );
-//     }
-// });
-
 import { createAPIClient } from '@shopware/api-client';
 
 /**
@@ -82,8 +9,7 @@ import { createAPIClient } from '@shopware/api-client';
  * The config for the apiClient is directly read from the runtimeConfig instead from
  * the apiBuilder include.
  *
- * The Cookie name that is set is changed from "sw-context-token" to "contextToken" because
- * this seems to be the one that Pond is using.
+ * The Cookie name that is set is changed from "sw-context-token" to "contextToken"
  */
 export default defineEventHandler(async event => {
     const body = await readBody(event);
@@ -118,6 +44,7 @@ export default defineEventHandler(async event => {
         await sendRedirect(event, '/', 400);
         return;
     }
+
     setCookie(
         event,
         'contextToken',
