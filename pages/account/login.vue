0e96d868b2882864446d1f05d624189b41273b2a
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-06-17 13:53:41 +0200

    POND-89: added additional line

--- pages/account/login.vue
+++ pages/account/login.vue
@@ -2,6 +2,7 @@
 const { rerouteIfLoggedIn } = usePondAuthentication();
 await rerouteIfLoggedIn();
 </script>
+
 <template>
     <div class="container relative flex justify-center py-4">
         <div class="w-full md:w-2/3 xl:w-1/2">
