40581dec1ed6f7a6cfd665d6a1eb460897976b09
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-06-20 11:28:15 +0200

    POND-46: added order page

---
+++ components/account/page/AccountPageOrder.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+const customerStore = useCustomerStore();
+const { orders } = storeToRefs(customerStore);
+</script>
+
+<template>
+  <AccountPageOrderInner v-if="orders" :orders="orders" />
+</template>
