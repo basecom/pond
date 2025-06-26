499e90473233f1b9d4548a2c6c199f3da7d0c2c2
JacquelineKlimmek <j.klimmek@basecom.de>
2025-06-26 07:35:47 +0200

    feat(header): Header optimizations (#171)
    * POND-63: added aria labels and ID s, fixed width for navigation levels, display of wishlist
    
    * POND-63: linter, adjusted modal mobile styling, added mobile account sidebar
    
    * POND-63: cr feedback
    
    * POND-63: fixed to path for locale links
    
    * POND-63: linter
    
    * POND-63: added default value
    
    * POND-63: outsourced account link
    
    * POND-63: added aria label and role
    
    * POND-63: linter after rebase
    
    * POND-63: removed lines

---
+++ components/account/AccountActionLinkInner.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+      link?: string;
+      label?: string;
+    }>(),
+    {
+        link: '/',
+        label: '',
+    },
+);
+
+defineEmits<{
+  click: [];
+}>();
+</script>
+
+<template>
+    <slot name="account-action-link">
+        <NuxtLinkLocale
+            :to="link"
+            :aria-label="label || 'Account action link'"
+            role="button"
+            @click="() => $emit('click')"
+        >
+            <slot name="account-action-link-inner">
+                <div class="flex cursor-pointer items-center gap-2 border-b-2 border-gray-100 py-3">
+                    {{ label }}
+                </div>
+            </slot>
+        </NuxtLinkLocale>
+    </slot>
+</template>
