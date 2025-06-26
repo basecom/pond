aca41c777e521b4e33242d979339908145b6f523
JacquelineKlimmek <j.klimmek@basecom.de>
2025-06-26 09:29:31 +0200

    feat(currency): Currency switcher (#175)
    * POND-26: added currency switcher components, added fetch all currencies composables
    
    * POND-26: added select
    
    * POND-26: Added code doc
    
    * POND-26: cr feedback
    
    * POND-26: mr feedback
    
    * POND-26: feedback
    
    * POND-26: added cursor

---
+++ components/CurrencySwitcher.vue
@@ -0,0 +1,10 @@
+<template>
+    <CurrencySwitcherInner>
+        <template #currency-switcher-trigger>
+            <slot name="currency-switcher-trigger" />
+        </template>
+        <template #currency-switcher-content>
+            <slot name="currency-switcher-content" />
+        </template>
+    </CurrencySwitcherInner>
+</template>
