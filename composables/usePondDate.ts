7628e41750b4c3844ad286645c801d95dbf64fce
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-06-26 08:57:23 +0200

    POND-46: corrected date format

--- composables/usePondDate.ts
+++ composables/usePondDate.ts
@@ -1,10 +1,24 @@
 export function usePondDate() {
     const { locale } = useI18n();
+    const { handleError } = usePondHandleError();
+
+    const localeFormats: {[key: string]: string;} = {
+        'de-DE': 'DD.MM.YYYY',
+        'en-GB': 'DD/MM/YYYY',
+    };
 
     const formatLocaleDate = (date: string) => {
-        if( locale.value === 'de-DE' || locale.value === 'en-GB') {
-            return useDateFormat(date, 'DD.MM.YYYY');
+        const format = localeFormats[locale.value];
+        if (format) {
+            try {
+                return useDateFormat(date, format);
+            } catch {
+                handleError('Invalid date format');
+                return undefined;
+            }
         }
+
+        return undefined;
     };
 
     return { formatLocaleDate };
