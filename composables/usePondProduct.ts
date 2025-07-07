371ad646a61d58c88b9cb6f88739c026fe5bdfd1
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-07-07 16:06:53 +0200

    POND-7: corrected cache name

--- composables/usePondProduct.ts
+++ composables/usePondProduct.ts
@@ -2,7 +2,7 @@ export function usePondProduct() {
     const { apiClient } = useShopwareContext();
     const { handleError } = usePondHandleError();
 
-    const getProductsById = async (itemIds: string[]) => usePondCacheAsyncData(`products-${itemIds}`, async () => {
+    const getProductsById = async (itemIds: string[]) => usePondCacheAsyncData(`products-${itemIds.join('-')}`, async () => {
         try {
             return (await apiClient.invoke('readProduct post /product', {
                 body: { ids: itemIds },
