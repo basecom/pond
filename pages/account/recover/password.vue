4673f1c6ffc99993986bd832d5f58e173af27eee
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-07-08 16:27:16 +0200

    POND-59: refactored password recover page, added hash validation, added password recover page

---
+++ pages/account/recover/password.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useToast } from '@/components/ui/toast/use-toast';
+
+const { toast } = useToast();
+const route = useRoute();
+const { t } = useI18n();
+
+const hashQuery: Ref<undefined | string> = ref(route.query.hash?.toString() || undefined);
+
+onMounted(() => {
+  if(!hashQuery.value) {
+    toast({
+      title: t('account.recover.hash.missing'),
+    });
+  }
+})
+
+</script>
+
+<template>
+  <div class="container relative flex justify-center py-4">
+    <div class="w-full md:w-2/3 xl:w-1/2">
+      <AccountPageRecoverPassword v-if="hashQuery" />
+      <AccountPageRecover v-else />
+    </div>
+  </div>
+</template>
