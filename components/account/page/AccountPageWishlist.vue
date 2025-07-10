f637341340b7333677d4f6e9f4f8c5c63aa1a62e
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-07-10 11:47:38 +0200

    PONDD-7: added try catch block

--- components/account/page/AccountPageWishlist.vue
+++ components/account/page/AccountPageWishlist.vue
@@ -8,6 +8,7 @@ const {
 const { getProductsById } = usePondProduct();
 const route = useRoute();
 const router = useRouter();
+const { handleError } = usePondHandleError();
 
 const isLoading = ref(true);
 const wishlistProducts = ref(undefined);
@@ -34,20 +35,26 @@ onMounted(async () => {
 
 const changePage = async (page: number) => {
     isLoading.value = true;
-    await router.push({
-        query: {
-            page,
-            limit: defaultLimit.value,
-        },
-    });
-    await getWishlistProducts(route.query);
 
-    const { data } = await getProductsById(items.value);
+    try {
+        await router.push({
+            query: {
+                page,
+                limit: defaultLimit.value,
+            },
+        });
+        await getWishlistProducts(route.query);
 
-    if (data.value) {
-        wishlistProducts.value = data.value.elements;
+        const { data } = await getProductsById(items.value);
+
+        if (data.value) {
+            wishlistProducts.value = data.value.elements;
+        }
+    } catch (error) {
+        handleError(error);
+    } finally {
+        isLoading.value = false;
     }
-    isLoading.value = false;
 };
 </script>
 
