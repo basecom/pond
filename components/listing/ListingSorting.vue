a0b7ebb3c1043753c2059d6db81afa1d4cc5c657
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-08-06 14:32:16 +0200

    POND-35: cr feedback

--- components/listing/ListingSorting.vue
+++ components/listing/ListingSorting.vue
@@ -10,30 +10,44 @@ const {
 } = useCategoryListing();
 const route = useRoute();
 const router = useRouter();
+const { handleError } = usePondHandleError();
+const { t } = useI18n();
+const { toast } = usePondToast();
 
 const selectSortingItems: Ref<undefined | SelectItems> = ref(undefined);
 
 onMounted(async () => {
-    selectSortingItems.value = getSortingOrders.value?.map(item => ({
-        value: item.key,
-        label: item.translated.label,
-    }));
+    if(getSortingOrders.value && getSortingOrders.value.length > 0) {
+        selectSortingItems.value = getSortingOrders.value?.map(item => ({
+            value: item.key,
+            label: item.translated.label,
+        }));
+    }
 });
 
 const currentSortingOrder = computed({
     get: (): string => getCurrentSortingOrder.value || '',
     set: async (order: string): Promise<void> => {
-        await router.push({
-            query: {
-                ...route.query,
-                order,
-            },
-        });
+        try {
+            await router.push({
+                query: {
+                    ...route.query,
+                    order,
+                },
+            });
 
-        await changeCurrentSortingOrder(order, {
-            ...(route.query as unknown as operations['searchPage post /search']['body']),
-            limit: route.query.limit ? Number(route.query.limit) : 15,
-        });
+            await changeCurrentSortingOrder(order, {
+                ...(route.query as unknown as operations['searchPage post /search']['body']),
+                limit: route.query.limit ? Number(route.query.limit) : 15,
+            });
+        } catch(error) {
+            toast({
+                title: t('general.errorHeadline'),
+                description: t('general.errorMessage'),
+                variant: 'destructive',
+            });
+            handleError(error);
+        }
     },
 });
 </script>
