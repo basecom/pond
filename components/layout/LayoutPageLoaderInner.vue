23670181bdfa25a816fd7817ea9c76412991e8f6
Jacqueline Klimmek <j.klimmek@basecom.de>
2025-08-07 11:43:07 +0200

    POND-81: fixed currency switch

---
+++ components/layout/LayoutPageLoaderInner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { getStyle } = usePondStyle();
+</script>
+
+<template>
+    <div :class="getStyle('layout.loader.outer')">
+        <Icon name="mdi:loading" :class="getStyle('layout.loader.icon')" />
+    </div>
+</template>
