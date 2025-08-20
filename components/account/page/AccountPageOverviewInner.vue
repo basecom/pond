3120f318dac84078fdce489777696a4d8f396ae5
neleruess <n.ruess@basecom.de>
2025-08-20 13:31:35 +0200

    fix bugs

--- components/account/page/AccountPageOverviewInner.vue
+++ components/account/page/AccountPageOverviewInner.vue
@@ -14,15 +14,12 @@ const { getNewsletterStatus, newsletterSubscribe, newsletterUnsubscribe } = useN
 const { handleError } = usePondHandleError();
 const { toast } = usePondToast();
 const { t } = useI18n();
-const runtimeConfig = useRuntimeConfig();
-const url = useRequestURL();
 
 const showTitle = configStore.get('core.loginRegistration.showTitleField') as boolean;
 const form$: Ref<null | Vueform> = ref(null);
 const newsletterStatus: Ref<undefined | Schemas['AccountNewsletterRecipient']> = ref(undefined);
 const displayDoubleNewsletterRegistrationAlert = ref(false);
 const isLoading = ref(false);
-const domainUrl = runtimeConfig.public.pond?.salesChannelDomain || url;
 
 onMounted(async () => {
     try {
@@ -89,7 +86,6 @@ const onChange = async (formData: NewsletterFormData) => {
                 street: props.customer.defaultBillingAddress?.street,
                 zipCode: props.customer.defaultBillingAddress?.zipcode,
                 city: props.customer.defaultBillingAddress?.city,
-                storefrontUrl: domainUrl,
             });
             toast({
                 title: t('newsletter.subscriptionConfirmationSuccess'),
